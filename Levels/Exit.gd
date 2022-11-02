ae0ab9990e1d538b8bba3472b7d40747a5a0cbf0
Noah Girdler <noahgirdler@gmail.com>
2022-11-02 19:59:20 -0400

    Completes Game

---
+++ Levels/Exit.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+func _on_Exit_body_entered(body):
+	if body.name == "Player":
+		if name == "Exit_to_2":
+			var _target = get_tree().change_scene("res://Levels/Game_Over.tscn")
---
+++ Levels/Game_Over.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+	
+func _on_Play_Again_pressed():
+	get_tree().change_scene("res://Levels/Level1.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
